77bf86dca5e7832f1c0cea48e567ee27889c1369
Ashfaq <105435085+Ashfaqbs@users.noreply.github.com>
2025-04-19 09:39:52 +0530

    Add Spring Boot main class

---
+++ rough/exception-handling-samples/src/main/java/com/example/ExceptionHandlingSamplesApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionHandlingSamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionHandlingSamplesApplication.class, args);
+	}
+
+}
