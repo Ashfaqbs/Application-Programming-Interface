2ffcb1d4efba7e8bdb23c3c940760e372cf1d82a
Ashfaq <105435085+Ashfaqbs@users.noreply.github.com>
2025-09-23 12:56:30 +0530

    Initialize Spring Boot application with configuration files

---
+++ springapiflow-example/src/main/java/com/example/demo/springapiflow_example/SpringapiflowExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.springapiflow_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class    SpringapiflowExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringapiflowExampleApplication.class, args);
+	}
+
+}
